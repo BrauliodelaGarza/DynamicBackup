61ddb3c3aae8f97f2f4ddb9307023005ca117a67
Braulio <meap08@hotmail.com>
2022-06-30 10:54:32 -0500

    error

---
+++ ConfigureDatabase/Models/Database.cs
@@ -0,0 +1,10 @@
+﻿namespace ConfigureDatabase.Models
+{
+    public class Database
+    {
+        public int id { get; set; }
+        public Conections connection { get; set; }
+        public string databasename { get; set; }
+        public int backupfrecuency { get; set; }
+    }
+}
